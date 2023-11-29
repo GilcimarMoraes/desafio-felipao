cb602d6a5c34db826582e2cb279c0b179f319764
GilcimarMoraes <myrraws@yahoo.com.br>
2023-11-29 01:01:27 -0300

    mudança saida

--- heroi.js
+++ heroi.js
@@ -4,30 +4,35 @@
 
 let heroi = "Spider man"
 let xp = 11000
+let nivel
 
 // Condições 
 
 if (xp <= 1000){
-    console.log("O heroi de nome " + heroi + " está no nível de Ferro")
+    nivel = "Ferro"
 }
 else if ((xp > 1000) && (xp <= 2000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Bronze")
+    nivel = "Bronze"
 }
 else if ((xp > 2000) && (xp <= 6000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Prata")
+    nivel = "Prata"
 }
 else if ((xp > 6000) && (xp <= 7000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Ouro")
+    nivel = "Ouro"
 }
 else if ((xp > 7000) && (xp <= 8000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Platina")
+    nivel = "Platina"
 }
 else if ((xp > 8000) && (xp <= 9000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Ascendente")
+    nivel = "Ascendente"
 }
 else if ((xp > 9000) && (xp <= 10000)){
-    console.log("O heroi de nome " + heroi + " está no nível de Imortal")
+    nivel = "Imortal"
 }
 else {
-    console.log("O heroi de nome " + heroi + " está no nível de Radiante")
+    nivel = "Radiante"
 }
+
+// Saida
+
+console.log("O Herói de nome " + heroi + " esta no nivel de " + nivel)
